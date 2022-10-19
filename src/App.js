abd97d824bc8ad18ba7f6c59a224dd044b93ef2b
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-10-19 15:56:11 +0300

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.png';
-import './App.css';
 
-function App() {
+import './App.css';
+import Dictionary from './Dictionary';
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className='App-footer'>Created by Iryna Polchaninova</footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
