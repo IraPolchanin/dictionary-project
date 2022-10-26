25f4f4814835bf8f2cb9955eeb4121f1f21d3723
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-10-26 12:07:20 +0300

    added Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
             {props.meaning.definitions.map(function (definition, index) {
                 return (
                     <div key={index}>
-                        <p>
-                            {definition.definition}
-                            <br />
-                            <em>{definition.example}</em>
-                        </p>
-
+                        <div className="definition">{definition.definition}</div>
+                        <strong>Example:</strong>
+                        <em>{definition.example}</em>
+                        <br />
                     </div>
-                )
+                );
             })}
+            <Synonyms synonyms={props.meaning.synonyms} />
         </div>
     );
 }
